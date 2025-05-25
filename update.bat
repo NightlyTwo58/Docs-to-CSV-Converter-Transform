0c44427fa48eed7953d25b03ca60b86339487005
Richard <qjscai@gmail.com>
2025-05-25 16:04:15 -0700

    Update 5/25/2025
    - edited update bat automation

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
 call mvn clean compile exec:java -Dexec.mainClass="org.example.Main"
-git add docs/output.csv docs/index.html
+git add docs/output.csv
 git commit -m "Daily update - %date%"
 git push origin main
